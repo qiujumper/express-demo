1a7b7fa5fd2cfc215c06e62e6c8475064abca57d
Jack Qiu <jack.qiu@frogdesign.com>
2017-02-02 21:27:34 +0800

    use chapter 11 code base

--- meadowlark.js
+++ meadowlark.js
@@ -1,11 +1,6 @@
-var http = require('http'),
-	express = require('express'),
+var express = require('express'),
 	fortune = require('./lib/fortune.js'),
-	formidable = require('formidable'),
-	fs = require('fs'),
-    bson = require('bson'),
-	Vacation = require('./models/vacation.js'),
-	VacationInSeasonListener = require('./models/vacationInSeasonListener.js');
+	formidable = require('formidable');
 
 var app = express();
 
@@ -29,143 +24,27 @@ app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-// use domains for better error handling
-app.use(function(req, res, next){
-    // create a domain for this request
-    var domain = require('domain').create();
-    // handle errors on this domain
-    domain.on('error', function(err){
-        console.error('DOMAIN ERROR CAUGHT\n', err.stack);
-        try {
-            // failsafe shutdown in 5 seconds
-            setTimeout(function(){
-                console.error('Failsafe shutdown.');
-                process.exit(1);
-            }, 5000);
-
-            // disconnect from the cluster
-            var worker = require('cluster').worker;
-            if(worker) worker.disconnect();
-
-            // stop taking new requests
-            server.close();
-
-            try {
-                // attempt to use Express error route
-                next(err);
-            } catch(error){
-                // if Express error route failed, try
-                // plain Node response
-                console.error('Express error mechanism failed.\n', error.stack);
-                res.statusCode = 500;
-                res.setHeader('content-type', 'text/plain');
-                res.end('Server error.');
-            }
-        } catch(error){
-            console.error('Unable to send 500 response.\n', error.stack);
-        }
-    });
-
-    // add the request and response objects to the domain
-    domain.add(req);
-    domain.add(res);
-
-    // execute the rest of the request chain in the domain
-    domain.run(next);
-});
-
-// logging
-switch(app.get('env')){
-    case 'development':
-    	// compact, colorful dev logging
-    	app.use(require('morgan')('dev'));
-        break;
-    case 'production':
-        // module 'express-logger' supports daily log rotation
-        app.use(require('express-logger')({ path: __dirname + '/log/requests.log'}));
-        break;
-}
-
-var MongoSessionStore = require('session-mongoose')(require('connect'));
-var sessionStore = new MongoSessionStore({ url: credentials.mongo[app.get('env')].connectionString });
-
 app.use(require('cookie-parser')(credentials.cookieSecret));
 app.use(require('express-session')({
     resave: false,
     saveUninitialized: false,
     secret: credentials.cookieSecret,
-	store: sessionStore,
 }));
 app.use(express.static(__dirname + '/public'));
 app.use(require('body-parser')());
 
-// database configuration
-var mongoose = require('mongoose');
-var options = {
-    server: {
-       socketOptions: { keepAlive: 1 } 
-    }
-};
 switch(app.get('env')){
     case 'development':
-        mongoose.connect(credentials.mongo.development.connectionString, options);
+        app.use(require('morgan')('dev')); console.log('I am dev');
         break;
+
     case 'production':
-        mongoose.connect(credentials.mongo.production.connectionString, options);
+        app.use(require('express-logger')({
+            path: __dirname + '/log/requests.log'
+        }));
+        console.log('I am production');
         break;
-    default:
-        throw new Error('Unknown execution environment: ' + app.get('env'));
 }
-
-// initialize vacations
-Vacation.find(function(err, vacations){
-    if(vacations.length) return;
-
-    new Vacation({
-        name: 'Hood River Day Trip',
-        slug: 'hood-river-day-trip',
-        category: 'Day Trip',
-        sku: 'HR199',
-        description: 'Spend a day sailing on the Columbia and ' + 
-            'enjoying craft beers in Hood River!',
-        priceInCents: 9995,
-        tags: ['day trip', 'hood river', 'sailing', 'windsurfing', 'breweries'],
-        inSeason: true,
-        maximumGuests: 16,
-        available: true,
-        packagesSold: 0,
-    }).save();
-
-    new Vacation({
-        name: 'Oregon Coast Getaway',
-        slug: 'oregon-coast-getaway',
-        category: 'Weekend Getaway',
-        sku: 'OC39',
-        description: 'Enjoy the ocean air and quaint coastal towns!',
-        priceInCents: 269995,
-        tags: ['weekend getaway', 'oregon coast', 'beachcombing'],
-        inSeason: false,
-        maximumGuests: 8,
-        available: true,
-        packagesSold: 0,
-    }).save();
-
-    new Vacation({
-        name: 'Rock Climbing in Bend',
-        slug: 'rock-climbing-in-bend',
-        category: 'Adventure',
-        sku: 'B99',
-        description: 'Experience the thrill of rock climbing in the high desert.',
-        priceInCents: 289995,
-        tags: ['weekend getaway', 'bend', 'high desert', 'rock climbing', 'hiking', 'skiing'],
-        inSeason: true,
-        requiresWaiver: true,
-        maximumGuests: 4,
-        available: false,
-        packagesSold: 0,
-        notes: 'The tour guide is currently recovering from a skiing accident.',
-    }).save();
-});
 
 // flash message middleware
 app.use(function(req, res, next){
@@ -228,8 +107,8 @@ app.get('/about', function(req,res){
 		pageTestScript: '/qa/tests-about.js' 
 	} );
 });
-app.get('/request-group-rate', function(req, res){
-	res.render('request-group-rate');
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
 });
 app.get('/jquery-test', function(req, res){
 	res.render('jquery-test');
@@ -259,6 +138,72 @@ NewsletterSignup.prototype.save = function(cb){
 	cb();
 };
 
+// mocking product database
+function Product(){
+}
+Product.find = function(conditions, fields, options, cb){
+	if(typeof conditions==='function') {
+		cb = conditions;
+		conditions = {};
+		fields = null;
+		options = {};
+	} else if(typeof fields==='function') {
+		cb = fields;
+		fields = null;
+		options = {};
+	} else if(typeof options==='function') {
+		cb = options;
+		options = {};
+	}
+	var products = [
+		{
+			name: 'Hood River Tour',
+			slug: 'hood-river',
+			category: 'tour',
+			maximumGuests: 15,
+			sku: 723,
+		},
+		{
+			name: 'Oregon Coast Tour',
+			slug: 'oregon-coast',
+			category: 'tour',
+			maximumGuests: 10,
+			sku: 446,
+		},
+		{
+			name: 'Rock Climbing in Bend',
+			slug: 'rock-climbing/bend',
+			category: 'adventure',
+			requiresWaiver: true,
+			maximumGuests: 4,
+			sku: 944,
+		}
+	];
+	cb(null, products.filter(function(p) {
+		if(conditions.category && p.category!==conditions.category) return false;
+		if(conditions.slug && p.slug!==conditions.slug) return false;
+		if(isFinite(conditions.sku) && p.sku!==Number(conditions.sku)) return false;
+		return true;
+	}));
+};
+Product.findOne = function(conditions, fields, options, cb){
+	if(typeof conditions==='function') {
+		cb = conditions;
+		conditions = {};
+		fields = null;
+		options = {};
+	} else if(typeof fields==='function') {
+		cb = fields;
+		fields = null;
+		options = {};
+	} else if(typeof options==='function') {
+		cb = options;
+		options = {};
+	}
+	Product.find(conditions, fields, options, function(err, products){
+		cb(err, products && products.length ? products[0] : null);
+	});
+};
 
 var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
 
@@ -296,116 +241,34 @@ app.post('/newsletter', function(req, res){
 app.get('/newsletter/archive', function(req, res){
 	res.render('newsletter/archive');
 });
-
 app.get('/contest/vacation-photo', function(req, res){
-	var now = new Date();
-	res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+    var now = new Date();
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
 });
-
-// make sure data directory exists
-var dataDir = __dirname + '/data';
-var vacationPhotoDir = dataDir + '/vacation-photo';
-if(!fs.existsSync(dataDir)) fs.mkdirSync(dataDir); 
-if(!fs.existsSync(vacationPhotoDir)) fs.mkdirSync(vacationPhotoDir);
-
-function saveContestEntry(contestName, email, year, month, photoPath){
-    // TODO...this will come later
-}
-
 app.post('/contest/vacation-photo/:year/:month', function(req, res){
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files){
-        if(err) {
-            req.session.flash = {
-                type: 'danger',
-                intro: 'Oops!',
-                message: 'There was an error processing your submission. ' +
-                    'Pelase try again.',
-            };
-            return res.redirect(303, '/contest/vacation-photo');
-        }
-        var photo = files.photo;
-        var dir = vacationPhotoDir + '/' + Date.now();
-        var path = dir + '/' + photo.name;
-        fs.mkdirSync(dir);
-        fs.renameSync(photo.path, dir + '/' + photo.name);
-        saveContestEntry('vacation-photo', fields.email,
-            req.params.year, req.params.month, path);
-        req.session.flash = {
-            type: 'success',
-            intro: 'Good luck!',
-            message: 'You have been entered into the contest.',
-        };
-        return res.redirect(303, '/contest/vacation-photo/entries');
+        if(err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
     });
 });
-
-app.get('/contest/vacation-photo/entries', function(req, res){
-	res.render('contest/vacation-photo/entries');
-});
-
-app.get('/vacation/:vacation', function(req, res, next){
-	Vacation.findOne({ slug: req.params.vacation }, function(err, vacation){
+app.get('/tours/:tour', function(req, res, next){
+	Product.findOne({ category: 'tour', slug: req.params.tour }, function(err, tour){
 		if(err) return next(err);
-		if(!vacation) return next();
-		res.render('vacation', { vacation: vacation });
+		if(!tour) return next();
+		res.render('tour', { tour: tour });
 	});
 });
-
-function convertFromUSD(value, currency){
-    switch(currency){
-    	case 'USD': return value * 1;
-        case 'GBP': return value * 0.6;
-        case 'BTC': return value * 0.0023707918444761;
-        default: return NaN;
-    }
-}
-
-app.get('/vacations', function(req, res){
-    Vacation.find({ available: true }, function(err, vacations){
-    	var currency = req.session.currency || 'USD';
-        var context = {
-            currency: currency,
-            vacations: vacations.map(function(vacation){
-                return {
-                    sku: vacation.sku,
-                    name: vacation.name,
-                    description: vacation.description,
-                    inSeason: vacation.inSeason,
-                    price: convertFromUSD(vacation.priceInCents/100, currency),
-                    qty: vacation.qty,
-                };
-            })
-        };
-        switch(currency){
-	    	case 'USD': context.currencyUSD = 'selected'; break;
-	        case 'GBP': context.currencyGBP = 'selected'; break;
-	        case 'BTC': context.currencyBTC = 'selected'; break;
-	    }
-        res.render('vacations', context);
-    });
-});
-
-app.post('/vacations', function(req, res){
-    Vacation.findOne({ sku: req.body.purchaseSku }, function(err, vacation){
-        if(err || !vacation) {
-            req.session.flash = {
-                type: 'warning',
-                intro: 'Ooops!',
-                message: 'Something went wrong with your reservation; ' +
-                    'please <a href="/contact">contact us</a>.',
-            };
-            return res.redirect(303, '/vacations');
-        }
-        vacation.packagesSold++;
-        vacation.save();
-        req.session.flash = {
-            type: 'success',
-            intro: 'Thank you!',
-            message: 'Your vacation has been booked.',
-        };
-        res.redirect(303, '/vacations');
-    });
+app.get('/adventures/:subcat/:name', function(req, res, next){
+	Product.findOne({ category: 'adventure', slug: req.params.subcat + '/' + req.params.name  }, function(err, adventure){
+		if(err) return next(err);
+		if(!adventure) return next();
+		res.render('adventure', { adventure: adventure });
+	});
 });
 
 var cartValidation = require('./lib/cartValidation.js');
@@ -413,37 +276,20 @@ var cartValidation = require('./lib/cartValidation.js');
 app.use(cartValidation.checkWaivers);
 app.use(cartValidation.checkGuestCounts);
 
-app.get('/cart/add', function(req, res, next){
-	var cart = req.session.cart || (req.session.cart = { items: [] });
-	Vacation.findOne({ sku: req.query.sku }, function(err, vacation){console.log('hereee');
-		if(err) return next(err);console.log('noot here?');
-		if(!vacation) return next(new Error('Unknown vacation SKU: ' + req.query.sku));
-		cart.items.push({
-			vacation: vacation,
-			guests: req.body.guests || 1,
-		});console.log(cart);
-		res.redirect(303, '/cart');
-	});
-});
 app.post('/cart/add', function(req, res, next){
-    console.log('here');
 	var cart = req.session.cart || (req.session.cart = { items: [] });
-	Vacation.findOne({ sku: req.body.sku }, function(err, vacation){
+	Product.findOne({ sku: req.body.sku }, function(err, product){
 		if(err) return next(err);
-		if(!vacation) return next(new Error('Unknown vacation SKU: ' + req.body.sku));
+		if(!product) return next(new Error('Unknown product SKU: ' + req.body.sku));
 		cart.items.push({
-			vacation: vacation,
-			guests: req.body.guests || 1,
-		});console.log(cart); console.log('good to go');
+			product: product,
+			guests: req.body.guests || 0,
+		});
 		res.redirect(303, '/cart');
 	});
 });
-
-
-
-
-app.get('/cart', function(req, res, next){console.log('welll');
-	var cart = req.session.cart;console.log(req.session); console.log('ok');
+app.get('/cart', function(req, res, next){
+	var cart = req.session.cart;
 	if(!cart) next();
 	res.render('cart', { cart: cart });
 });
@@ -481,45 +327,6 @@ app.post('/cart/checkout', function(req, res){
     res.render('cart-thank-you', { cart: cart });
 });
 
-app.get('/notify-me-when-in-season', function(req, res){
-    res.render('notify-me-when-in-season', { sku: req.query.sku });
-});
-
-app.post('/notify-me-when-in-season', function(req, res){
-    VacationInSeasonListener.update(
-        { email: req.body.email }, 
-        { $push: { skus: req.body.sku } },
-        { upsert: true },
-	    function(err){
-	        if(err) {
-	        	console.error(err.stack);
-	            req.session.flash = {
-	                type: 'danger',
-	                intro: 'Ooops!',
-	                message: 'There was an error processing your request.',
-	            };
-	            return res.redirect(303, '/vacations');
-	        }
-	        req.session.flash = {
-	            type: 'success',
-	            intro: 'Thank you!',
-	            message: 'You will be notified when this vacation is in season.',
-	        };
-	        return res.redirect(303, '/vacations');
-	    }
-	);
-});
-
-app.get('/set-currency/:currency', function(req,res){
-    req.session.currency = req.params.currency;
-    return res.redirect(303, '/vacations');
-});
-
-app.get('/epic-fail', function(req, res){
-    process.nextTick(function(){
-        throw new Error('Kaboom!');
-    });
-});
 
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
@@ -534,20 +341,7 @@ app.use(function(err, req, res, next){
 	res.render('500');
 });
 
-var server;
-
-function startServer() {
-    server = http.createServer(app).listen(app.get('port'), function(){
-      console.log( 'Express started in ' + app.get('env') +
-        ' mode on http://localhost:' + app.get('port') +
-        '; press Ctrl-C to terminate.' );
-    });
-}
-
-if(require.main === module){
-    // application run directly; start app server
-    startServer();
-} else {
-    // application imported as a module via "require": export function to create server
-    module.exports = startServer;
-}
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:' + 
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
