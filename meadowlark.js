e42ed3af431b54f88f1645d2b48710029d92e2d4
Jack Qiu <jack.qiu@frogdesign.com>
2017-02-03 16:23:55 +0800

    add auto view machenism

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 var express = require('express'),
 	fortune = require('./lib/fortune.js'),
     mongoose = require('mongoose'),
+    fs = require('fs'),
     Vacation = require('./models/vacation.js'),
     VacationInSeasonListener = require('./models/vacationInSeasonListener.js'),
 	formidable = require('formidable');
@@ -470,7 +471,24 @@ app.get('/epic-fail', function(){
     process.nextTick(function(){
         throw new Error('Hooooo!');
     });
-})
+});
+
+// add support for auto views
+var autoViews = {};
+
+app.use(function(req,res,next){ console.log('hi jack');
+    var path = req.path.toLowerCase();  
+    // check cache; if it's there, render the view
+    if(autoViews[path]) return res.render(autoViews[path]);
+    // if it's not in the cache, see if there's
+    // a .handlebars file that matches
+    if(fs.existsSync(__dirname + '/views' + path + '.handlebars')){
+        autoViews[path] = path.replace(/^\//, '');
+        return res.render(autoViews[path]);
+    }
+    // no view found; pass on to 404 handler
+    next();
+});
 
 // 404 catch-all handler (middleware)
 // there is no err param for 404
